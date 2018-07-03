db2a6a707daf081c13c910dbbaa390fe43340c05
markclynch <mark.christinat@gmail.com>
2018-07-03 01:31:30 -0700

    formatting

--- src/App.js
+++ src/App.js
@@ -1,74 +1,62 @@
-import React, { Component } from 'react';
-import Fan from './Components/Fan';
+import React, { Component } from "react";
+import Fan from "./Components/Fan";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      
-        temp: 78,
-        hotOrNot: false,
-        isOn: false
-      
-    }
+      temp: 78,
+      hotOrNot: false,
+      isOn: false
+    };
   }
-  
-    
+
   componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-    
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
   tick = () => {
     this.setState({
-      
-       temp: this.state.temp + Math.floor((Math.random() * 4) -1)     
-      
+      temp: this.state.temp + Math.floor(Math.random() * 4 - 1)
     });
-  }
-  
-  spinHandler(){
-    
-    if(this.state.isOn) {
+  };
+
+  spinHandler() {
+    if (this.state.isOn) {
       clearInterval(this.timer2);
     } else {
-      this.timer2 = setInterval(this.tick2, 600)
+      this.timer2 = setInterval(this.tick2, 600);
     }
     this.setState({
       isOn: !this.state.isOn
-    })
+    });
   }
 
   tick2 = () => {
     if (this.state.isOn) {
-    this.setState({
-      temp: this.state.temp -1
-    });
-  }
-  }
-  
+      this.setState({
+        temp: this.state.temp - 1
+      });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <Fan temp={this.state.temp} />
-        
-          <div>on/off</div>
-          <img onClick = {() => this.spinHandler()} 
-          alt="logo" 
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" 
-          className = {`App-logo  ${!this.state.isOn ? `pause` : ``}`} 
-           />
-        
+
+        <div>on/off</div>
+        <img
+          onClick={() => this.spinHandler()}
+          alt="logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+          className={`App-logo  ${!this.state.isOn ? `pause` : ``}`}
+        />
       </div>
     );
   }
 }
 
 export default App;
-
-
--- src/Components/Fan.js
+++ src/Components/Fan.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Fan extends Component {
-
-  
   render() {
-    
     return (
       <div>
         {this.props.temp > 82 ? (
-          <button className="button" style={{backgroundColor:"red"}}>{this.props.temp}</button>
+          <button className="button" style={{ backgroundColor: "red" }}>
+            {this.props.temp}
+          </button>
         ) : (
-          <button className="button" style={{backgroundColor:"green"}}>{this.props.temp}</button>
+          <button className="button" style={{ backgroundColor: "green" }}>
+            {this.props.temp}
+          </button>
         )}
-        
-      
       </div>
     );
   }
